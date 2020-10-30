e1bf00807d3ddc786d2d7acc097cbefd2daf5462
Jeffrey Chiu <mr.jeffrey.chiu@gmail.com>
2020-10-30 17:52:55 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,11 +1,11 @@
 def find_max_value(array)
-  count = 0
-  max_value = 0
-  while count < array.length do
+  count = 0 
+  max_value = -1
+  while count < array.lenth do
     if max_value < array[count]
       max_value = array[count]
-    end
-    count += 1
+    end 
+    count +=
   end
   max_value
 end
